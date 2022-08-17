6bee5cc44aece51a4ffe53fbb70e07afbe8be5c4
dcunhamaciel <dcunhamaciel@gmail.com>
2022-08-17 19:22:53 -0300

    Métodos HTTP

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -2,8 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello World Ignite!" });
+app.get("/courses", (request, response) => {
+    return response.json(["curso 1", "curso 2", "curso 3"]);
+});
+
+app.post("/courses", (request, response) => {
+    return response.json(["curso 1", "curso 2", "curso 3", "curso 4"]);
+});
+
+app.put("/courses/:id", (request, response) => {
+    return response.json(["curso 6", "curso 2", "curso 3", "curso 4"]);
+});
+
+app.patch("/courses/:id", (request, response) => {
+    return response.json(["curso 6", "curso 7", "curso 3", "curso 4"]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json(["curso 6", "curso 7", "curso 4"]);
 });
 
 app.listen(3333);
