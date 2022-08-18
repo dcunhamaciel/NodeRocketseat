ef9257b0b9bb08cdb82132b958d2f3d31850c41a
Diego Cunha Maciel <diegocunha@diretriz.corp>
2022-08-18 09:05:51 -0300

    Tipos de Parâmetros

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -2,15 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/courses", (request, response) => {
+    const query = request.query;
+    console.log(query);
     return response.json(["curso 1", "curso 2", "curso 3"]);
 });
 
 app.post("/courses", (request, response) => {
+    const body = request.body;
+    console.log(body);
     return response.json(["curso 1", "curso 2", "curso 3", "curso 4"]);
 });
 
 app.put("/courses/:id", (request, response) => {
+    const params = request.params;
+    console.log(params);
     return response.json(["curso 6", "curso 2", "curso 3", "curso 4"]);
 });
 
