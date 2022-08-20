a558624a7c738bfe1aad11eea2005079f9331f6b
dcunhamaciel <dcunhamaciel@gmail.com>
2022-08-20 13:00:42 -0300

    Middlewares

--- 02.fin_api/src/index.js
+++ 02.fin_api/src/index.js
@@ -7,12 +7,23 @@ app.use(express.json());
 
 const customers = [];
 
-/**
- * id - uuid
- * cpf - string
- * name - string
- * statement - []
- */
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find(
+        (customer) => customer.cpf === cpf
+    );
+
+    if (!customer) {
+        return response.status(400).json({ error: "Customer not found" });
+    }
+
+    request.customer = customer;
+    
+    return next();
+}
+
 app.post("/account", (request, response) => {  
     const { cpf, name } = request.body;
 
@@ -34,16 +45,8 @@ app.post("/account", (request, response) => {
     return response.status(201).send();
 });
 
-app.get("/statement", (request, response) => {
-    const { cpf } = request.headers;
-
-    const customer = customers.find(
-        (customer) => customer.cpf === cpf
-    );
-
-    if (!customer) {
-        return response.status(400).json({ error: "Customer not found" });
-    }
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
 
     return response.json(customer.statement);
 });
