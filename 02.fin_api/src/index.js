aca66c73cd663f88f2daf1073d04787e900c557c
dcunhamaciel <dcunhamaciel@gmail.com>
2022-08-20 12:28:23 -0300

    Cadastro de Conta

--- 02.fin_api/src/index.js
+++ 02.fin_api/src/index.js
@@ -1,5 +1,31 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+/**
+ * id - uuid
+ * cpf - string
+ * name - string
+ * statement - []
+ */
+app.post("/account", (request, response) => {
+    const id = uuidv4();
+
+    const { cpf, name } = request.body;
+
+    customers.push({
+        id,
+        cpf,
+        name,
+        statement: []
+    });
+
+    return response.status(201).send();
+});
 
 app.listen(3333);
