9483efd1c33c0b2d7eb7644794217f69c5e0c94c
dcunhamaciel <dcunhamaciel@gmail.com>
2022-08-20 13:07:53 -0300

    Criando depósito na conta

--- 02.fin_api/src/index.js
+++ 02.fin_api/src/index.js
@@ -51,4 +51,21 @@ app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
     return response.json(customer.statement);
 });
 
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    const { description, amount } = request.body;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
+
 app.listen(3333);
